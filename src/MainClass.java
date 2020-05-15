992734b8cb3a800462a02231450e4e01abe5fd65
Bodi <mohamed474792@hotmail.com>
2020-05-16 00:28:47 +0200

    fix

--- src/MainClass.java
+++ src/MainClass.java
@@ -12,6 +12,9 @@ public class MainClass
     static JFrame win = new JFrame("Title");
     static JPanel pan = new JPanel();
     static JButton but = new JButton("send");
+    static JTextField text = new JTextField("");
+    static JTextArea chatlog = new JTextArea("");
+    static JScrollPane scrollPane = new JScrollPane(chatlog);
     static Connection connection = new Connection();
     static Seeker seeker = new Seeker();
     static boolean connected = false;
@@ -28,8 +31,16 @@ public class MainClass
         win.setSize(500, 500);
         win.setLocationRelativeTo(null);
         win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pan.setLayout(null);
         win.add(pan);
         pan.add(but);
+        pan.add(text);
+        pan.add(scrollPane);
+        but.setBounds(225, 50, 50, 20);
+        text.setBounds(50, 80, 400, 20);
+        scrollPane.setBounds(50,120,400,200);
+        chatlog.setEditable(false);
+        but.setMargin(new Insets(0,0,0,0));
         but.addActionListener(new SendAction());
         but.setEnabled(false);
         seek.start();

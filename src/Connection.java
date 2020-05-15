41e97fae77b727bb6b8a39ace5230d3eb85da7dd
Bodi <mohamed474792@hotmail.com>
2020-05-15 23:54:36 +0200

    fix

--- src/Connection.java
+++ src/Connection.java
@@ -31,7 +31,7 @@ public class Connection implements Runnable
             try
             {
                 System.out.println("retrying");
-                setup(new Socket(MainClass.ipAddress, MainClass.port2));
+                setup(new Socket(MainClass.ipAddress, MainClass.port2), false);
             }catch (Exception e)
             {
                 System.out.println("Timout");
@@ -39,28 +39,37 @@ public class Connection implements Runnable
         }
     }
 
-    public static void setup(Socket client) throws Exception
+    public static void setup(Socket client, boolean server) throws Exception
     {
+        if (!server)
+        {
+            MainClass.seek.interrupt();
+        }
         MainClass.connection.client = client;
         in = client.getInputStream();
         out = client.getOutputStream();
         System.out.println("now connected");
         MainClass.begin();
-        MainClass.seek.interrupt();
+
         while (MainClass.connected && in.read(buffer) != -1)
         {
-            System.out.println(new String(buffer));
+            readMessage(buffer);
+            buffer = new byte[1000];
         }
         System.out.println("quit");
     }
 
-    private static void readMessage()
+    private static void readMessage(byte[] message)
     {
-
+        System.out.println(new String(message));
     }
+
     public static void sendMessage(String message) throws Exception
     {
         System.out.println("jsalkdjl");
+        buffer = message.getBytes();
+        out.write(buffer);
+        buffer = new byte[1000];
     }
 
 }
